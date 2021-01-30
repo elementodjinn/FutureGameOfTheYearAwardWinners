f5a293aed9bfdf1b4b906108284ed0926a39e82e
Ahlon Sin <ahls@uci.edu>
2021-01-29 20:22:44 -0800

    added player health scripts

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    #region variables
+    [SerializeField]
+    private int healthMax;
+    public int healthCurrent { get; set; }
+    public bool isDead { get; set; }
+
+    #region MonoBehaviour callbacks
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthCurrent = healthMax;
+    }
+    #endregion
+
+
+    public void takeDamage(int amount)
+    {
+        healthCurrent -= amount;
+        if(healthCurrent <= 0)
+        {
+            onDeath();
+        }
+    }
+
+    private void onDeath()
+    {
+        isDead = true;
+        ///add more stuff here
+    }
+}
