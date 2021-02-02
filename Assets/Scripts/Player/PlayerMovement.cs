c335e3fcfb8841214689b5ab0a05fd4b0c9c17ea
Ahlon Sin <ahls@uci.edu>
2021-02-01 19:04:54 -0800

    applied animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,20 +10,27 @@ public class PlayerMovement : MonoBehaviour
     //This is just a general speed multiplier for the player, haven't tried anything else yet.
     public float speed;
 
+    float moveHorizontal = 0f;
+    float moveVertical = 0f;
+
     private Rigidbody2D playerRigidbody;
+
+    private Animator animator;
+    private SpriteRenderer SR;
 
     #endregion
 
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        SR = GetComponent<SpriteRenderer>();
+
     }
-
-    void FixedUpdate()
+    private void Update()
     {
-        float moveHorizontal = 0f;
-        float moveVertical = 0f;
-
+        moveHorizontal = 0f;
+        moveVertical = 0f;
         if (Input.GetKey("a"))
         {
             moveHorizontal = -1f;
@@ -47,9 +54,24 @@ public class PlayerMovement : MonoBehaviour
             moveVertical *= .7f;
         }
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
+        //Animation part
+        animator.SetFloat("VerticalSpeed", moveVertical);
+        animator.SetFloat("HorizontalSpeed", moveHorizontal);
+        if (moveHorizontal > 0)
+        {
+            SR.flipX = false;
+        }
+        else if (moveHorizontal < 0)
+        {
+            SR.flipX = true;
+        }
+
+    }
+    void FixedUpdate()
+    {
         //setting velocity because I don't know how to make smooth acceleration with a maximum velocity. 
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         playerRigidbody.velocity = movement * speed;
     }
 }
