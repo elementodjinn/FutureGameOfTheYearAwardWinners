bdfe338e2f1857e79d5c3c0de928a3b554f3dab7
Ahlon Sin <ahls@uci.edu>
2021-02-02 20:39:10 -0800

    created a crude title. basic networking implemented

---
+++ Assets/PhotonConnectionManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+public class PhotonConnectionManager : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+    #region variables
+    private string playerName;
+    public InputField playerNameInput;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    public void Connect()
+    {
+        playerName = playerNameInput.text;
+        PhotonNetwork.NickName = playerName;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    #region PUN callbacks
+
+    private void OnConnectedToServer()
+    {
+        Debug.Log("Connected To server");
+        PhotonNetwork.GameVersion = "0.1";
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Master");
+        PhotonNetwork.GameVersion = "0.0";
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions{MaxPlayers = 2});
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room");
+        PhotonNetwork.LoadLevel("Sin Scene");
+    }
+    #endregion
+}
---
+++ Assets/PhotonPlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonPlayerManager : MonoBehaviour
+{
+    #region variables
+
+    private PhotonView PV;
+    public GameObject playerPrefab;
+    private GameObject playerInstance;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInstance = PhotonNetwork.Instantiate(playerPrefab.name, Vector2.zero, Quaternion.identity,0);
+        playerInstance.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = PhotonNetwork.NickName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
