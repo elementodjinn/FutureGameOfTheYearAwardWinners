bf5aefa9ffc70fc3b4439d2ef50b856be25ed6d0
Alex Meng <alexmeng99@gmail.com>
2021-02-03 04:31:06 -0800

    fixed enemyMovement. ants now patrol, and follow player when in radius.

---
+++ Assets/Scripts/Enemy/NPC.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Character //inheriting from character class
+{
+    public virtual void DeSelect()
+    {
+      
+    }
+
+    public virtual Transform Select()
+    {
+        return hitBox;
+    }
+}
---
+++ Assets/Scripts/Enemy/Range.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // if we hit a player, follow him
+
+    private Enemy parent;
+
+    private void Start()
+    {
+        parent = GetComponentInParent<Enemy>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            parent.Target = collision.transform;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            parent.Target = null;
+
+        }
+    }
+}
