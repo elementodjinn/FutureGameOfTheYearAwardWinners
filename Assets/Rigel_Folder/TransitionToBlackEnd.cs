29382baf56e62d064af9c46eab563176c03931e8
Rigel Kuper <rkuper@uci.edu>
2021-03-18 19:06:08 -0700

    Added end scene and added a trigger so when the player hits it, the player will be taken to the end scene

---
+++ Assets/Rigel_Folder/TransitionToBlackEnd.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionToBlackEnd : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        anim.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1f);
+
+        //change to the end scene
+        SceneManager.LoadScene("EndScene");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(FadeToBlack());
+        }
+    }
+}
